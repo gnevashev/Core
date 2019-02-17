b721acff656b7d06684e6fe5cb206119109d362e
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2019-02-17 13:42:37 +0100

    Added new column to a table t_CoreTable

--- Tables/t_CoreTable.sql
+++ Tables/t_CoreTable.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[t_CoreTable]
 (
 	[Id] INT NOT NULL identity PRIMARY KEY,
-	CoreCaption nvarchar (255) not null
+	CoreCaption nvarchar (255) not null, 
+    [CoreSettings] NVARCHAR(50) NULL
 )

b2d764337fe205d7c7090e98ad75e6174e216433
Evgeny Gnevashev <evgeny.gnevashev@gmail.com>
2019-02-19 22:59:10 +0100

    Test more than one table data populating

---
+++ DeployData/t_CoreTable2.PostDeployment.sql
@@ -0,0 +1,24 @@
+﻿:setvar XMLDATA "'"
+
+DECLARE @json_t_CoreTable2 nvarchar(max) = 
+$(XMLDATA)
+:r ..\Data\t_CoreTable2.json
+$(XMLDATA)
+
+
+set identity_insert t_CoreTable2 on
+merge into [dbo].t_CoreTable2 as tc
+  using (
+	select Id, TypeOper, Caption
+	from openjson (@json_t_CoreTable2)  
+	with (   
+			Id int '$.Id'
+)) s (Id) ON s.Id = tc.Id
+when matched then update set TypeOper = s.TypeOper, Caption = s.Caption
+
+when not matched by target
+then insert (Id)
+values (Id)
+
+when not matched by source then delete;
+set identity_insert t_CoreTable2 off
---
+++ DeployData/top_Chains.PostDeployment.sql
@@ -0,0 +1,30 @@
+﻿:setvar XMLDATA "'"
+
+DECLARE @json_top_Chains nvarchar(max) = 
+$(XMLDATA)
+:r ..\Data\top_Chains.json
+$(XMLDATA)
+
+
+set identity_insert top_Chains on
+merge into [dbo].top_Chains as tc
+  using (
+	select Id, TypeOper, Caption
+	from openjson (@json_top_Chains)  
+	with (   
+				Id int '$.Id'
+			, TypeOper int '$.TypeOper'
+			, Caption nvarchar (50) '$.Caption'
+)) s (Id, TypeOper, Caption) ON s.Id = tc.Id
+when matched and (
+        s.TypeOper <> tc.TypeOper
+        or not ((s.Caption = tc.Caption and not (s.Caption IS NULL OR tc.Caption IS NULL)) OR (s.Caption IS NULL AND tc.Caption IS NULL))
+    )
+then update set TypeOper = s.TypeOper, Caption = s.Caption
+
+when not matched by target
+then insert (Id, TypeOper, Caption)
+values (Id, TypeOper, Caption)
+
+when not matched by source then delete;
+set identity_insert top_Chains off
